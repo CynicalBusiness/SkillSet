d3fa01583e2ff70e83f6f4b8309c657962e35292
Matthew Struble <Matt@MayateckInnovations.net>
2014-09-13 07:52:03 -0700

    Added a basic Skills object and system.

--- src/me/capit/classes/Skills.java
+++ src/me/capit/classes/Skills.java
@@ -1,14 +1,42 @@
 package me.capit.classes;
 
+import java.io.Serializable;
+import java.util.HashMap;
 import java.util.UUID;
 
-public class Skills {
+public class Skills implements Serializable {
+	private static final long serialVersionUID = 5004667268740769774L;
+	
 	final UUID uuid;
 	final SkillSetMain plugin;
+	
+	private HashMap<String, Integer> levels = new HashMap<String, Integer>();
 	
 	public Skills(SkillSetMain plugin, UUID uuid){
 		this.plugin = plugin;
 		this.uuid = uuid;
+		
+	}
+	
+	public int getSkillValue(String name){
+		if (levels.containsKey(name)){
+			return levels.get(name);
+		} else {
+			return 0;
+		}
+	}
+	
+	public void setSkillValue(String name, int value){
+		levels.put(name, value);
+	}
+	
+	public void incrementSkillValue(String name, int amount){
+		int val = getSkillValue(name);
+		levels.put(name, val+amount);
+	}
+	
+	public void incrementSkillValue(String name){
+		incrementSkillValue(name,1);
 	}
 	
 }
